0e9f85c6a745085973b6ad7a596cbf24274fe79b
simpic-official <officialsimpic@gmail.com>
2024-08-06 21:47:02 +0700

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -215,23 +215,49 @@ export const resources = [
 
 export const faqs = [
     {
-        question: "what's the most frequently asked question?",
-        answer: "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list",
+        question: "What are the registration fees included?",
+        answer: "Registration fees cover all expenses from January 18th to 21st, 2025, encompassing food, lodging, tour activities, and more. In summary, no additional charges apply for attending SIMPIC following registration.",
         id: "f1",
     },
     {
-        question: "what's the most frequently asked question?",
-        answer: "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list",
+        question: "Do the team need to stay at the provided hotel during the competition?",
+        answer: "Please note that our partner hotel is optional. Your team can also choose not to stay at the provided hotel during this competition. If your team chooses not to stay at our provided hotel, you need to select the package with no hotel room during the registration process.",
         id: "f2"
     },
     {
-        question: "what's the most frequently asked question?",
-        answer: "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list",
+        question: "For each university, how many teams can participate in the competition?",
+        answer: "There is no restriction on the number of teams that a university can enter into the competition.",
         id: "f3"
     },
     {
-        question: "what's the most frequently asked question?",
-        answer: "Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list",
+        question: "Can clinical medical students join the competition?",
+        answer: "Absolutely! Clinical medical students are welcome to partake in the competition, provided they are enrolled in their respective universities.",
         id: "f4"
     },
+    {
+        question: "Are there any requirements or essays needed in order to join the competition?",
+        answer: "Only a letter of approval (or "statement") and PDPA consent are required for SIMPIC registration which can be downloaded from the website.",
+        id: "f5"
+    },
+    {
+        question: "Can participants modify their registration details, such as team composition or accommodation preferences, after they have registered?",
+        answer: "Participants are welcome to modify their registration details, including team composition and accommodation preferences, before finalizing and submitting their application. It's important to review and ensure accuracy before hitting the "submit" button, as changes cannot be accommodated after submission.",
+        id: "f6"
+    },
+    {
+        question: "What are check-in and check-out times for accommodations?",
+        answer: "For the upcoming event. It is our pleasure to inform you that transportation will indeed be provided for all activities scheduled within the competition venue, tour activities, and the exam itself. This is to ensure a seamless experience for all participants. However, we recommend that participants personally handle transportation arrangements from the airport. This will allow competitors to immerse themselves in the Thai experience and tailor their transportation and schedule according to their flight and preferences.",
+        id: "f7"
+    },
+    {
+        question: "Will transportation be provided for the tour activities, or should participants arrange their own transportation?",
+        answer: "Delegates can start checking in at 13.00 on January 17, 2025 (GMT+7)",
+        id: "f8"
+    },
+    {
+        question: "What is the cancellation policy for the event? Will participants receive a refund if they are unable to attend after registering?",
+        answer: "Inform you that we have a strict no-refund policy for this event. We understand that unexpected circumstances can arise, but the logistics and planning involved in organizing the event prevent us from offering refunds to participants who are unable to attend after registering. We've taken this decision to ensure the fairness and consistency of our policy for all participants. While we cannot provide refunds, we do encourage participants to consider their availability and commitment before registering. If you have any further questions or concerns, please feel free to reach out to us, and we'd be happy to assist you.",
+        id: "f9"
+    },
+    
 ]
