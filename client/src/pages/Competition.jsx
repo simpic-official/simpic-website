6909e6aaae176ba7424229151dc83bf132a0c13d
simpic-official <officialsimpic@gmail.com>
2024-08-06 23:06:37 +0700

    Update Competition.jsx

--- client/src/pages/Competition.jsx
+++ client/src/pages/Competition.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 import RefMat from "../images/RefMat.png";
 import Prize from "../images/IP.png";
 
 const Competition = () => {
   return (
-    <section className="mt-[10em] w-screen h-screen flex flex-col items-center font-inter gap-[20em]">
-      <div className='w-[80%] flex justify-center'>
+    <section className="mt-40 w-screen h-screen flex flex-col items-center font-inter gap-32">
+      <div className='w-4/5 flex flex-col items-center'>
         <p className='text-6xl font-bold'>Competition</p>
       </div>
-      <div className='w-[80%] flex justify-center'>
+      <div className='w-4/5 flex flex-col items-center'>
         <p className='text-6xl font-bold'>Prize</p>
-        <img src={Prize} alt="Prize" className="mt-8 w-full h-auto" />
+        <div className='w-full flex justify-center mt-8'>
+          <img src={Prize} alt="Prize" className="w-3/5 h-auto" />
+        </div>
       </div>
       <div className='w-4/5 flex flex-col items-center'>
         <p className='text-6xl font-bold'>Reference Material</p>
-        <img src={RefMat} alt="Reference Material" className="mt-8 w-full h-auto" />
+        <div className='w-full flex justify-center mt-8'>
+          <img src={RefMat} alt="Reference Material" className="w-3/5 h-auto" />
+        </div>
       </div>
     </section>
   )
 }
 
-export default Competition
+export default Competition;
