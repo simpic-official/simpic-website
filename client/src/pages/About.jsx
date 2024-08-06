e5b5e960a6df25e2b08988193449297e573e9301
simpic-official <officialsimpic@gmail.com>
2024-08-07 02:37:22 +0700

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,21 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import President from '../images/President.jpg';
 import SIMPICHis1 from '../images/SIMPICHis-1.png';
 import SIMPICHis2 from '../images/SIMPICHis-2.png';
 import SIMPICHis3 from '../images/SIMPICHis-3.png';
 
 const About = () => {
-  const [open, setOpen] = useState({
-    simpic2024: false,
-    simpic2023: false,
-    simpic2022: false,
-    simpic2021: false,
-  });
-
-  const changeDisplay = (id) => {
-    setOpen((prevOpen) => ({ ...prevOpen, [id]: !prevOpen[id] }));
-  };
-
   return (
     <div className='mt-[15em] w-screen h-full flex justify-center'>
       <div className='w-[75vw]'>
@@ -108,37 +97,16 @@ const About = () => {
         <section className='flex flex-col mt-[10em]'>
           <p className='text-7xl font-bold text-center'>SIMPIC History</p>
           <div className='w-full flex flex-col items-center'>
-            <div className='w-[720px] h-auto'>
+            <div className='w-[512px] h-auto'>
               <img src={SIMPICHis1} alt='SIMPIC History 1' className='w-full h-auto object-cover' />
             </div>
-            <div className='w-[720px] h-auto'>
+            <div className='w-[512px] h-auto'>
               <img src={SIMPICHis2} alt='SIMPIC History 2' className='w-full h-auto object-cover' />
             </div>
-            <div className='w-[720px] h-auto mb-4'>
+            <div className='w-[512px] h-auto mb-4'>
               <img src={SIMPICHis3} alt='SIMPIC History 3' className='w-full h-auto object-cover' />
             </div>
           </div>
-        </section>
-        <section className='flex flex-col items-center mt-[10em] gap-5'>
-          {['simpic2024', 'simpic2023', 'simpic2022', 'simpic2021'].map((id) => (
-            <div
-              key={id}
-              className={`transition-all duration-300 ease-in-out w-full max-w-[516px] hover:-translate-y-1 hover:scale-105 ${
-                open[id] ? 'max-h-[129px]' : 'max-h-[84px]'
-              } flex gap-3 items-center px-4 py-4 bg-gradient-to-r from-[rgba(251,166,166,1)] to-[rgba(232,201,122,1)] rounded-lg cursor-pointer`}
-              onClick={() => changeDisplay(id)}
-            >
-              <p className='flex items-center text-[24px] font-bold'>{id.toUpperCase()}</p>
-              <p className={`${open[id] ? 'w-[1px] h-[80px] bg-black' : 'hidden'}`}></p>
-              <p className={`${open[id] ? 'block text-xs lg:text-md' : 'hidden'}`}>
-                Journey into a small world of
-                <br />
-                microbio,
-                <br />
-                immuno, and parasito
-              </p>
-            </div>
-          ))}
         </section>
       </div>
     </div>
