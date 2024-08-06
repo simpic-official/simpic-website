33fc07f2296a8bdefd6b62c583f7a751047cf39f
simpic-official <officialsimpic@gmail.com>
2024-08-06 23:10:31 +0700

    Update Competition.jsx

--- client/src/pages/Competition.jsx
+++ client/src/pages/Competition.jsx
@@ -4,20 +4,20 @@ import Prize from "../images/IP.png";
 
 const Competition = () => {
   return (
-    <section className="mt-40 w-screen h-screen flex flex-col items-center font-inter gap-32">
-      <div className='w-4/5 flex flex-col items-center'>
-        <p className='text-6xl font-bold'>Competition</p>
+    <section className="mt-40 w-screen h-screen flex flex-col items-center font-inter gap-32 px-4 md:px-8 lg:px-16">
+      <div className='w-full md:w-4/5 flex flex-col items-center'>
+        <p className='text-4xl md:text-6xl font-bold'>Competition</p>
       </div>
-      <div className='w-4/5 flex flex-col items-center'>
-        <p className='text-6xl font-bold'>Prize</p>
+      <div className='w-full md:w-4/5 flex flex-col items-center'>
+        <p className='text-4xl md:text-6xl font-bold'>Prize</p>
         <div className='w-full flex justify-center mt-8'>
-          <img src={Prize} alt="Prize" className="w-3/5 h-auto" />
+          <img src={Prize} alt="Prize" className="w-full md:w-4/5 h-auto" />
         </div>
       </div>
-      <div className='w-4/5 flex flex-col items-center'>
-        <p className='text-6xl font-bold'>Reference Material</p>
+      <div className='w-full md:w-4/5 flex flex-col items-center'>
+        <p className='text-4xl md:text-6xl font-bold'>Reference Material</p>
         <div className='w-full flex justify-center mt-8'>
-          <img src={RefMat} alt="Reference Material" className="w-3/5 h-auto" />
+          <img src={RefMat} alt="Reference Material" className="w-full md:w-4/5 h-auto" />
         </div>
       </div>
     </section>
