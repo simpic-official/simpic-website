587ece8c45382e29bfce9393fff810fd1f0bde66
simpic-official <officialsimpic@gmail.com>
2024-08-13 22:02:48 +0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,28 +5,9 @@ import '../Home.css';
 
 const Home = ({ user }) => {
   const [mouseEnter, setMouseEnter] = useState("zero");
-  const [bgColor, setBgColor] = useState("bg-[rgba(217,217,217,1)]");
-
-  useEffect(() => {
-    if (mouseEnter === "one") {
-      setBgColor("bg-[rgb(239,238,220)]");
-    } else if (mouseEnter === "two") {
-      setBgColor("bg-[rgb(220,233,194)]");
-    } else if (mouseEnter === "three") {
-      setBgColor("bg-[rgb(211,219,252)]");
-    } else if (mouseEnter === "four") {
-      setBgColor("bg-[rgb(251,234,254)]");
-    } else if (mouseEnter === "five") {
-      setBgColor("bg-[rgb(223,241,254)]");
-    } else if (mouseEnter === "six") {
-      setBgColor("bg-[rgb(123,152,117)]");
-    } else {
-      setBgColor("bg-[rgba(217,217,217,1)]");
-    }
-  }, [mouseEnter]);
 
   return (
-    <div className={`home-header min-w-screen min-h-screen ${bgColor} flex flex-col items-center`}>
+    <div className={`home-header min-w-screen min-h-screen flex flex-col items-center`}>
       <VideoBackground /> 
 
       <div className="absolute text-center flex flex-col gap-3">
