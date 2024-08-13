3610d7d19c180e42263b7bb13a7be2f3581b3aef
simpic-official <officialsimpic@gmail.com>
2024-08-13 21:10:33 +0700

    Update VideoBackground.js

--- client/src/components/VideoBackground.js
+++ client/src/components/VideoBackground.js
@@ -1,11 +1,11 @@
 // components/VideoBackground.js
 import React from 'react';
-import styles from './VideoBackground.module.css';
+import '../VideoBackground.css'; 
 
 const VideoBackground = () => {
   return (
-    <div className={styles.videoContainer}>
-      <video autoPlay loop muted className={styles.video}>
+    <div className="videoContainer">
+      <video autoPlay loop muted className="video">
         <source src="/videos/background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
